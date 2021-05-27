511b3e84a8e3b0d2aa132748dc2e6bba9f0dfa48
Maciek Grzela <maciekgrzela45@gmail.com>
2021-05-27 21:48:35 +0200

    WelcomeView Added

--- src/Pages/Welcome/Welcome.js
+++ src/Pages/Welcome/Welcome.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import { FiArrowDown } from 'react-icons/fi';
 const Welcome = () => {
-  return <p>Welcome Page!</p>;
+  return (
+    <div className='welcome-page'>
+      <div className='welcome-page__banner banner'>
+        <div className='banner__container'>
+          <h2 className='banner__title'>Przygotuj się na zimowe szaleństwo!</h2>
+          <h4 className='banner__desc'>Teraz wszystko do 60% taniej!</h4>
+          <button className='banner__go-to-store'>
+            Przejdź do sklepu <FiArrowDown />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
