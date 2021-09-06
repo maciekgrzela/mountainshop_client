a53aab51cb7c96478f9ee9885c185ef438db3d3e
Maciek Grzela <maciekgrzela45@gmail.com>
2021-09-06 18:54:13 +0200

    Code Structure Cleaning

--- src/Pages/Access/SignIn.js
+++ src/Pages/Access/SignIn.js
@@ -6,7 +6,7 @@ const SignIn = () => {
   return (
     <div className='sign-in-page'>
       <div className='sign-in-page__form-container'>
-        <div style={{ display: 'flex' }}>
+        <div className='d-flex'>
           <div>
             <h2>Zaloguj się...</h2>
             <SignInForm />
